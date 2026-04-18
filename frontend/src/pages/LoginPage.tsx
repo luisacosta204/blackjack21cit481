57b96b2674a5df2e673736c4326261941fce0bee
A123Kruse <143438676+A123Kruse@users.noreply.github.com>
2026-04-17 17:53:57 -0700

    login update

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./LoginPage.css";
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
